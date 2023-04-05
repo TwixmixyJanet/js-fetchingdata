9a1b12ea42bacbe03df7423e86dcabb61e35ef9b
Janet Webster <twixmixy@gmail.com>
2023-04-05 16:04:53 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const getData = async function () {
+  const res = await fetch("https://api.quotable.io/quotes?author=maya-angelou");
+  const data = await res.json();
+  console.log(data);
+};
+getData();
